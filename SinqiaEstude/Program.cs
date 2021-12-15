57a239c7f76880684dc153cf03360eff2e4ee552
Jonathabreu <jonathabreu2021@outlook.com>
2021-12-15 11:30:02 -0300

    Alteração de menu

--- SinqiaEstude/Program.cs
+++ SinqiaEstude/Program.cs
@@ -32,7 +32,7 @@ namespace AppSinqiaEstude
             while (continuaMenu == true)
             {
                 Thread.Sleep(500);
-                Console.WriteLine("\n--Bem vindo ao Sistema Sinqia Estude--\nDigite 1 para cadastro \nDigite 2 para excluir\n Digite 3 para sair");
+                Console.WriteLine("\n--Bem vindo ao Sistema Sinqia Estude--\nDigite 1 para Gestão de aluno \nDigite 2 para Gestão de professores \n Digite 3 para Gestão de matérias \n Digite 4 para Gestão financeira");
                 menu = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("menu: "+menu);
                 continua = true;
@@ -43,25 +43,9 @@ namespace AppSinqiaEstude
                         while (continua == true)
                         {
                             Thread.Sleep(500);
-                            Console.WriteLine("\nDigite 1 para cadastrar professor \nDigite 2 para cadastrar Aluno \nDigite 3 para cadastrar matéria\nDigite 4 para atribuir matéria para o Aluno");
+                            Console.WriteLine("\n Digite 1 para cadastrar Aluno \nDigite 2 para Excluir aluno \nDigite 3 para atribuir matéria para o Aluno");
                             escolha = Convert.ToInt32(Console.ReadLine());
-                            if (escolha == 1)//cadastrar professor
-                            {
-                                Console.WriteLine("Digite o nome do Professor: ");
-                                nome = Console.ReadLine();
-                                
-                                Console.WriteLine("Digite a idade do Professor: ");
-                                idade = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("Digite o cpf do Professor: ");
-                                cpf = Console.ReadLine();
-                                Console.WriteLine("Digite o email do Professor: ");
-                                email = Console.ReadLine();
-                                Console.WriteLine("Digite o endereço do Professor: ");
-                                endereco = Console.ReadLine();
-                                professorService.cadastrarProfessor(nome, idade, email, cpf, endereco);
-                                continua = false;
-                            }
-                            else if (escolha == 2)//cadastrar aluno
+                            if (escolha == 1)//cadastrar aluno
                             {
                                 Console.WriteLine("Digite o nome do Aluno: ");
                                 nome = Console.ReadLine();
@@ -76,14 +60,13 @@ namespace AppSinqiaEstude
                                 alunoService.cadastrarAluno(nome, idade, email, cpf, endereco);
                                 continua = false;
                             }
-                            else if (escolha == 3)// cadastrar materia
+                            else if (escolha == 2)//excluir aluno
                             {
-                                Console.WriteLine("Digite o nome da materia: ");
-                                nome = Console.ReadLine();
-                                materiaService.cadastrarMateria(nome);
-                                continua = false;   
+
+                                
                             }
-                            else if (escolha == 4)//atribuir matéria para o aluno
+                           
+                            else if (escolha == 3)//atribuir matéria para o aluno
                             {
                                 Console.WriteLine("Digite o nome do Aluno: ");
                                 nome = Console.ReadLine();
@@ -104,6 +87,58 @@ namespace AppSinqiaEstude
 
                         }
                         break;
+
+                    case 2:
+
+                        while (continua == true)
+                        {
+                            Thread.Sleep(500);
+                            Console.WriteLine("\n Digite 1 para cadastrar Professor \nDigite 2 para Excluir professor \nDigite 3 para atribuir matéria para o Professor");
+                            escolha = Convert.ToInt32(Console.ReadLine());
+                            if (escolha == 1)//cadastrar professor
+                            {
+                                Console.WriteLine("Digite o nome do Professor: ");
+                                nome = Console.ReadLine();
+
+                                Console.WriteLine("Digite a idade do Professor: ");
+                                idade = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine("Digite o cpf do Professor: ");
+                                cpf = Console.ReadLine();
+                                Console.WriteLine("Digite o email do Professor: ");
+                                email = Console.ReadLine();
+                                Console.WriteLine("Digite o endereço do Professor: ");
+                                endereco = Console.ReadLine();
+                                professorService.cadastrarProfessor(nome, idade, email, cpf, endereco);
+                                continua = false;
+                            }
+                            else if (escolha == 2)//excluir professor
+                            {
+
+                                
+                            }
+      
+                            else if (escolha == 3)//atribuir matéria para o professor
+                            {
+                                Console.WriteLine("Digite o nome do Aluno: ");
+                                nome = Console.ReadLine();
+                                Console.WriteLine("Digite o nome da matéria: ");
+                                nomeMateria = Console.ReadLine();
+
+                                alunoService.cadastrarMateriaAluno(nome, nomeMateria);
+                                if (alunoService.statusAtribuicaoMateria)
+                                {
+                                    continua = false;
+                                }
+                            }
+                            else
+                            {
+                                Thread.Sleep(500);
+                                Console.WriteLine("Escolha inválida! ");
+                            }
+
+                        }
+                        break;
+
                     case 3:
                         continuaMenu = false;
                         break;
